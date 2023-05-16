ba33bd7c69a4a9469c59a2e2a6b4b89d7bc7498e
OldSinner <antylifepatia@gmail.com>
2023-05-16 20:05:55 +0200

    getting neighbor

--- Map.js
+++ Map.js
@@ -4,7 +4,7 @@ class Map {
     for (let i = 0; i < x; i++) {
       this.cells[i] = [];
       for (let j = 0; j < y; j++) {
-        this.cells[i][j] = new Cell(i, j, cellsize);
+        this.cells[i][j] = new Cell(i, j, cellsize, this);
       }
     }
   }
@@ -19,18 +19,86 @@ class Map {
 }
 
 class Cell {
-  constructor(x, y, cellsize) {
+  constructor(x, y, cellsize, mapref) {
     this.x = x;
     this.y = y;
     this.cellsize = cellsize;
+    this.visited = false;
+    this.walls = [4];
+    this.mapref = mapref;
+    for (let i = 0; i < 4; i++) {
+      this.walls[i] = true;
+    }
   }
+  getRandomNeighbor() {
+    let neighbors = [];
+    if (this.x > 0 && this.mapref.cells[this.x - 1][this.y].visited == false) {
+      neighbors.push([this.x - 1, this.y]);
+    }
+    if (
+      this.x < map.cells.length - 1 &&
+      this.mapref.cells[this.x + 1][this.y].visited == false
+    ) {
+      neighbors.push([this.x + 1, this.y]);
+    }
+    if (this.y > 0 && this.mapref.cells[this.x][this.y - 1].visited == false) {
+      neighbors.push([this.x, this.y - 1]);
+    }
+    if (
+      this.y < map.cells[0].length - 1 &&
+      this.mapref.cells[this.x][this.y + 1].visited == false
+    ) {
+      neighbors.push([this.x, this.y + 1]);
+    }
+    if (neighbors.length == 0) {
+      return null;
+    }
+    return neighbors[Math.floor(Math.random() * neighbors.length)];
+  }
+
   drawCell() {
-    fill(255);
+    if (this.visited) {
+      fill(0, 255, 0);
+    } else {
+      fill(0, 0, 255);
+    }
     rect(
       this.x * this.cellsize,
       this.y * this.cellsize,
       this.cellsize,
       this.cellsize
     );
+    if (this.walls[0]) {
+      line(
+        this.x * this.cellsize,
+        this.y * this.cellsize,
+        (this.x + 1) * this.cellsize,
+        this.y * this.cellsize
+      );
+    }
+    if (this.walls[1]) {
+      line(
+        (this.x + 1) * this.cellsize,
+        this.y * this.cellsize,
+        (this.x + 1) * this.cellsize,
+        (this.y + 1) * this.cellsize
+      );
+    }
+    if (this.walls[2]) {
+      line(
+        this.x * this.cellsize,
+        (this.y + 1) * this.cellsize,
+        (this.x + 1) * this.cellsize,
+        (this.y + 1) * this.cellsize
+      );
+    }
+    if (this.walls[3]) {
+      line(
+        this.x * this.cellsize,
+        this.y * this.cellsize,
+        this.x * this.cellsize,
+        (this.y + 1) * this.cellsize
+      );
+    }
   }
 }
